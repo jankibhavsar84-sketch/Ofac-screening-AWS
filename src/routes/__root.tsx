c58ea3f6cf91c7c0229de591ce33412f67b4827f
Janki Bhavsar <jankibhavsar84@gmail.com>
2026-02-22 18:47:10 -0500

    Can you move  "OFAC Screening" header on top of Page to align on top of status card like attached image

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,22 +9,24 @@ function RootLayout() {
   return (
     <div className="appShell">
       <header className="topHeader">
-        <div className="brandRow">
-          <div className="brandIcon" aria-hidden="true">🛡️</div>
-          <div className="brandText">
-            <div className="brandTitle">OFAC Screening</div>
-            <div className="brandSub">Sanctions compliance screening platform</div>
+        <div className="topHeaderInner page">
+          <div className="brandRow">
+            <div className="brandIcon" aria-hidden="true">{"\u{1F6E1}\uFE0F"}</div>
+            <div className="brandText">
+              <div className="brandTitle">OFAC Screening</div>
+              <div className="brandSub">Sanctions compliance screening platform</div>
+            </div>
           </div>
-        </div>
 
-        <nav className="topNav">
-          <Link to="/intro" className="navLink" activeProps={{ className: "navLink active" }}>
-            Introduction
-          </Link>
-          <Link to="/screening" className="navLink" activeProps={{ className: "navLink active" }}>
-            Screening
-          </Link>
-        </nav>
+          <nav className="topNav">
+            <Link to="/intro" className="navLink" activeProps={{ className: "navLink active" }}>
+              Introduction
+            </Link>
+            <Link to="/screening" className="navLink" activeProps={{ className: "navLink active" }}>
+              Screening
+            </Link>
+          </nav>
+        </div>
       </header>
 
       <main className="pageMain">
